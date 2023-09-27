f1a893e938b4e519d3f5a1ce3be90199bacff82a
Valentin Duffet <vavadu07@gmail.com>
2023-09-27 14:07:40 +0200

    add search filter current employee

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,6 +1,20 @@
+import { useSelector } from 'react-redux';
 import RowBodyElement from './RowBodyElement/RowBodyElement';
 import styles from './Table.module.scss';
 import TableHeaderElement from './TableHeaderElement/TableHeaderElement';
+import { RootState } from '../../utils/store';
+
+type employeeType = {
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+  startDate: string;
+  street: string;
+  city: string;
+  stateAdress: string;
+  zip: string;
+  department: string;
+};
 
 const Table = () => {
   const titleHeaderElement: Array<string> = [
@@ -16,18 +30,29 @@ const Table = () => {
   ];
   const getLocalStorage = localStorage.getItem('employeesList')!;
   const getListEmployee = JSON.parse(getLocalStorage);
+  const filterTableValue = useSelector(
+    (state: RootState) => state.dataFilterTableCurrentEmployee
+  );
+  let filteredTable;
 
-  type employeeType = {
-    firstName: string;
-    lastName: string;
-    dateOfBirth: string;
-    startDate: string;
-    street: string;
-    city: string;
-    stateAdress: string;
-    zip: string;
-    department: string;
+  const filterSearch = (list: Array<employeeType>) => {
+    filteredTable = list.filter((employee: employeeType) => {
+      const keyValue: Array<string> = Object.keys(employee);
+      const data: Array<string> = [];
+
+      keyValue.map((value: string) => {
+        data.push(employee[value].toLowerCase());
+      });
+
+      return data.toString().includes(filterTableValue.search);
+    });
   };
+
+  if (filterTableValue.search.length > 0) {
+    filterSearch(getListEmployee);
+  } else if (filterTableValue.search.length === 0) {
+    filteredTable = getListEmployee;
+  }
 
   return (
     <table className={styles['table']}>
@@ -39,9 +64,14 @@ const Table = () => {
         </tr>
       </thead>
       <tbody>
-        {getListEmployee?.map((employee: employeeType, i: number) => (
+        {filteredTable?.map((employee: employeeType, i: number) => (
           <RowBodyElement employee={employee} key={i} />
         ))}
+        {filteredTable.length === 0 && (
+          <tr>
+            <th colSpan={9}>No matching records found</th>
+          </tr>
+        )}
       </tbody>
     </table>
   );
